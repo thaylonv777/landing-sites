0841256c97ebd66dcbc9fbe861ba938fc54ff5c8
thaylonv777 <SETE7MARKETING.UP@GMAIL.COM>
2026-08-11 17:27:12 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import Script from "next/script";
 import "./globals.css";
 
 export const metadata = {
-  metadataBase: new URL("https://sites.atriomarketing.site"),
+  metadataBase: new URL("[https://sites.atriomarketing.site](https://sites.atriomarketing.site)"),
   title: "Átrio · Sites que aparecem no Google",
   description:
     "Criação de sites institucionais e landing pages com SEO profissional. Sites rápidos, sob medida e construídos para serem encontrados por quem procura pelo seu negócio.",
@@ -10,8 +10,8 @@ export const metadata = {
     "criação de sites, site com SEO, site profissional, landing page, agência de sites, site otimizado Google, Átrio Marketing",
   openGraph: {
     type: "website",
-    locale: "pt_BR",
-    url: "https://sites.atriomarketing.site",
+    locale: "pt\_BR",
+    url: "[https://sites.atriomarketing.site](https://sites.atriomarketing.site)",
     title: "Átrio · Sites que aparecem no Google",
     description:
       "Sites rápidos, sob medida e com SEO profissional. Construídos para serem encontrados.",
@@ -30,29 +30,39 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="[https://fonts.googleapis.com](https://fonts.googleapis.com)" />
+        <link rel="preconnect" href="[https://fonts.gstatic.com](https://fonts.gstatic.com)" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;600;700&family=Inter:wght@400;500;600&display=swap"
+          href="[https://fonts.googleapis.com/css2?family=Space+Grotesk\:wght@500;600;700&family=Inter\:wght@400;500;600&display=swap](https://fonts.googleapis.com/css2?family=Space+Grotesk\:wght@500;600;700\&family=Inter\:wght@400;500;600\&display=swap)"
           rel="stylesheet"
         />
-            <!-- Meta Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '5403951239830409');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=5403951239830409&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Meta Pixel Code -->
+
+        {/* Meta Pixel Code */}
+        <Script id="facebook-pixel">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '5403951239830409');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="[https://www.facebook.com/tr?id=5403951239830409&ev=PageView&noscript=1](https://www.facebook.com/tr?id=5403951239830409\&ev=PageView\&noscript=1)"
+          />
+        </noscript>
+
+        {/* End Meta Pixel Code */}
       </head>
       <body>{children}</body>
     </html>
