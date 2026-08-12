03c36cc119eba61c41f5b3f798ed1c23be982af6
thaylonv777 <SETE7MARKETING.UP@GMAIL.COM>
2026-08-11 23:24:34 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -70,6 +70,15 @@ export default function RootLayout({ children }) {
         </noscript>
 
         {/* End Meta Pixel Code */}
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JHV36SF6QF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JHV36SF6QF');
+</script>
       </head>
       <body>{children}</body>
     </html>
